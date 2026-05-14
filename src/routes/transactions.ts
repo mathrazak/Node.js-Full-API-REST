c59e0a6db78d8c3b73172acb2335b31df941c621
mathrazak <matheus.sobreiro.fdsm@gmail.com>
2026-05-14 17:10:58 -0300

    add List transactions, Get transaction and Get summary

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,13 +4,39 @@ import { randomUUID } from 'node:crypto'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.post('/', async (request, reply) => {
-    // { title, amount, type: credit ou debit }
 
-    const createTransactionBodySchema = z.object({
-      title: z.string(),
-      amount: z.number(),
-      type: z.enum(['credit', 'debit']),
+    app.get('/', async () => {
+        const transactions = await knex ('transactions').select()
+
+        return { transactions }
+    })
+
+    app.get('/:id', async (request) => {
+        const getTransactionParamsSchema = z.object({
+            id: z.string().uuid(),
+        })
+
+        const { id } = getTransactionParamsSchema.parse(request.params)
+
+        const transaction = await knex('transactions').where('id', id).first()
+
+        return { transaction }
+    })
+
+    app.get('/summary', async () => {
+        const summary = await knex('transactions')
+        .sum('amount',  { as: 'amount' })
+        .first()
+
+        return { summary }
+    })
+    
+    app.post('/', async (request, reply) => {
+
+        const createTransactionBodySchema = z.object({
+        title: z.string(),
+        amount: z.number(),
+        type: z.enum(['credit', 'debit']),
     })
 
     const { title, amount, type } = createTransactionBodySchema.parse(
